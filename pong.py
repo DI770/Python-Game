c2846103aeeb7a5569d31a7168421e8cb80b9e1f
DI770 <peter.karlsson1987@gmail.com>
2022-12-21 22:13:55 +0000

    ball bounce

--- pong.py
+++ pong.py
@@ -1,47 +1,47 @@
 import turtle
 
 BACKGROUND_COLOR = "black"
-SCREEN_HEIGHT = 1000
-SCREEN_WIDTH = 1000
+SCREEN_HEIGHT = 700
+SCREEN_WIDTH = 700
 
-# Window
+#Window
 window = turtle.Screen()
 window.title("Pong")
 window.bgcolor(BACKGROUND_COLOR)
 window.setup(SCREEN_HEIGHT, SCREEN_WIDTH)
 window.tracer(0)
 
-# Paddel 1
+#Paddel 1
 paddle_1 = turtle.Turtle()
 paddle_1.speed(0)
 paddle_1.shape('triangle')
 paddle_1.color('green')
-paddle_1.shapesize(stretch_len=5, stretch_wid=10)
+paddle_1.shapesize(stretch_len=1, stretch_wid=5)
 paddle_1.penup()
-paddle_1.goto(-450, 0)
+paddle_1.goto(-350, 0)
 
-# Paddel 2
+#Paddel 2
 paddle_2 = turtle.Turtle()
 paddle_2.speed(0)
-paddle_2.shape('triangle')
+paddle_2.shape('square')
 paddle_2.color('green')
-paddle_2.shapesize(stretch_len=5, stretch_wid=10)
+paddle_2.shapesize(stretch_len=1, stretch_wid=5)
 paddle_2.tilt(180)
 paddle_2.penup()
-paddle_2.goto(450, 0)
+paddle_2.goto(350, 0)
 
-# Ball
+#Ball
 ball = turtle.Turtle()
 ball.speed(0)
 ball.shape('circle')
 ball.color('green')
 ball.penup()
 ball.goto(0, 0)
-ball.ax = 0.7
-ball.ay = 0.7
+ball.ax = 0.3
+ball.ay = -0.3
 
 
-# Paddle Mechanics
+#Paddle Mechanics
 def paddle_1_up():
     y = paddle_1.ycor()
     y += 20
@@ -77,3 +77,21 @@ while True:
 
     ball.setx(ball.xcor() + ball.ax)
     ball.sety(ball.ycor() + ball.ay)
+
+    # Border bounce
+    if ball.ycor() > 340:
+        ball.sety(340)
+        ball.ay *= -1
+
+    if ball.ycor() < -340:
+        ball.sety(-340)
+        ball.ay *= -1
+
+    if ball.xcor() > 351:
+        ball.goto(0, 0)
+        ball.ax *= -1
+
+    if ball.xcor() < -351:
+        ball.goto(0, 0)
+        ball.ax *= -1
+        
